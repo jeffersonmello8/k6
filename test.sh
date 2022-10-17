30b5386259e53e3196145a1cdd559832329b1526
Jefferson Melo <jefferson.mello8@hotmail.com>
2022-10-17 08:34:27 -0300

    primeiros testes construídos

---
+++ test.sh
@@ -0,0 +1,4 @@
+curl --request POST \
+     --url http://localhost:3333/signup \
+     --header 'Content-Type: application/json' \
+     --data '{"email": "jamile.lima@hotmail.com", "password": "pwd123"}'
