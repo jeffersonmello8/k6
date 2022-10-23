ef52cef06159933957b6636ba50d3ffe6e6d9dc0
Jefferson Melo <jefferson.mello8@hotmail.com>
2022-10-23 15:19:51 -0300

    diminuindo o tempo de teste do smoke test

--- tests/signup-smoke.js
+++ tests/signup-smoke.js
@@ -5,7 +5,7 @@ import uuid from './libs/uuid.js';
 
 export const options = {
   vus: 1,
-  duration: '1m',
+  duration: '30s',
   thresholds: {
     http_req_duration: ['p(95)<2000'], // 95% das requests devem responder em até 2s
     http_req_failed:['rate<0.01'] // 1% das requisições podem ocorrer erro
